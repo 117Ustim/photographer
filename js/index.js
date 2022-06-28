f57c846197375fe2f9371d9e7d7535a9fa326804
117Ustim <96074208+117Ustim@users.noreply.github.com>
2022-06-28 12:45:55 +0300

    28.06.22

---
+++ js/index.js
@@ -0,0 +1,11 @@
+let swiper = new Swiper(".mySwiper", {
+  // updateOnWindowResize: false,
+  autoHeight: true,
+  // setWrapperSize: false,
+    slidesPerView: 3,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
