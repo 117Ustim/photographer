37fd466dcd5b6c29343d1b824832bf3c5e10f3fa
117Ustim <96074208+117Ustim@users.noreply.github.com>
2022-07-01 15:51:39 +0300

    01.07.22

---
+++ js/admin.js
@@ -0,0 +1,26 @@
+
+const openPopUp = document.querySelector('.open_pop_up');
+const closePopUp = document.getElementById('pop_up_close');
+const popUp = document.getElementById('pop_up');
+
+openPopUp.addEventListener('click',function(e){
+e.preventDefault();
+popUp.classList.add('active');
+
+});
+
+closePopUp.addEventListener('click',() => {
+    popUp.classList.remove('active');  
+});
+
+document.getElementById('check').onclick = function() {
+let login = document.getElementById('login').value;
+let password = document.getElementById('password').value;
+if (login == 'admin' && password == '777'){
+    alert('Welcome');
+ }   else {
+        alert('Вы ввели неправельный логин или пароль');
+    }
+
+
+};
