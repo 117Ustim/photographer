c973b13af63d79d473a0cb9541a31fa03113db31
sergo <sergo.ngn@gmail.com>
2022-07-05 14:06:15 +0300

    redirect to admin page

--- js/admin.js
+++ js/admin.js
@@ -17,7 +17,7 @@ document.getElementById('check').onclick = function() {
 let login = document.getElementById('login').value;
 let password = document.getElementById('password').value;
 if (login == 'admin' && password == '777'){
-    document.location.href = "admin.html";
+    window.location.href = "admin.html";
  }   else {
         alert('Вы ввели неправельный логин или пароль');
     }
